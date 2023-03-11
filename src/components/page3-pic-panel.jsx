8ac0629fc0c42a746d94d30c2f2c2745164b1195
mjsirfan2015 <mohammedirfan2012india@gmail.com>
2023-03-11 23:34:53 +0530

    added responsiveness

--- src/components/page3-pic-panel.jsx
+++ src/components/page3-pic-panel.jsx
@@ -11,14 +11,14 @@ export default function Page3PicPanel({duration,images:{image1,image2,image3,ima
                 <button className="text-green-800 font-bold text-sm">Change</button>
             </div>
             <div className="grid grid-rows-2 grid-flow-col gap-4 w-full h-[400px] px-10">
-                <div className="row-span-2 relative"><Image src={image6} fill className="rounded-md"/></div>
-                <div className="col-span-2 relative"><Image src={image2} fill className="rounded-md"/></div>
-                <div className="row-span-1 relative"><Image src={image1} fill className="rounded-md"/></div>
-                <div className="row-span-1 relative"><Image src={image3} fill className="rounded-md"/></div>
-                <div className="col-span-1 relative"><Image src={image4} fill className="rounded-md"/></div>
+                <div className="row-span-2 relative"><Image src={image6} alt="grid pictures" fill className="rounded-md"/></div>
+                <div className="col-span-2 relative"><Image src={image2} alt="grid pictures" fill className="rounded-md"/></div>
+                <div className="row-span-1 relative"><Image src={image1} alt="grid pictures" fill className="rounded-md"/></div>
+                <div className="row-span-1 relative"><Image src={image3} alt="grid pictures" fill className="rounded-md"/></div>
+                <div className="col-span-1 relative"><Image src={image4} alt="grid pictures" fill className="rounded-md"/></div>
                 <div className="col-span-1 relative overflow-hidden">
                 <div style={{borderRadius: '5px', overflow: 'hidden'}}>
-                    <Image src={image5} fill className="rounded-md overflow-hidden" style={{objectFit:'cover'}}/>
+                    <Image src={image5} alt="grid pictures" fill className="rounded-md overflow-hidden" style={{objectFit:'cover'}}/>
                 </div>
                     <div className="absolute w-full h-full z-10">
                         <div className="flex w-full h-full text-4xl text-white font-bold justify-center items-center">+{imagesRemaining}</div> 
