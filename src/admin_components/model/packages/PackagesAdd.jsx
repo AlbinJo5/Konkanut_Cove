e33cd27079ad7524db252df29eb8373aa09b615c
AlbinJo5 <albinjo12345alex@gmail.com>
2023-04-26 18:07:49 +0530

    pacakages basic

--- src/admin_components/model/packages/PackagesAdd.jsx
+++ src/admin_components/model/packages/PackagesAdd.jsx
@@ -2,30 +2,61 @@ import { Modal, useModal, Button, Text, Tooltip, Spacer, Checkbox } from "@nextu
 import { DeleteIcon, EditIcon, IconButton } from "../../icons";
 import { Input, Grid, Textarea } from "@nextui-org/react";
 import { useEffect, useState } from "react";
-import { addData } from "@/utils/firebaseDataHandler";
 import { useRouter } from "next/router"
+import { uploadImages } from "@/utils/firebase_image_upload";
+import { queryClient } from "@/pages/_app";
+import { uploadData } from "@/utils/firebase_data_handler";
 
-export default function PackageAdd(props) {
-    const [form, setForm] = useState({
-        createdAt:new Date()
-    });
-    const router = useRouter();
+export default function PackageAdd() {
     const { setVisible, bindings } = useModal();
-    const [imagePath, setImagePath] = useState([]);
 
-    if (props.visibity) {
-        setVisible(false);
-    }
+    const handleAdd = (data) => {
+        const resp = uploadData(data, "Packages")
+        resp.then(res => {
+            console.log(res);
+            if (res.message === "success") {
+                // update or add the response to the cache
+                queryClient.setQueryData(['packages'], (old) => {
+                    const oldData = old?.data
+                    if (oldData) {
+                        return { ...old, data: [...oldData, res.data] }
+                    }
+                    else {
+                        return { ...old, data: [res.data] }
+                    }
+                })
 
-    // Create New Product API
-    const createProduct = async (images, form) => {
-        form.isAvailable = true;
-        await addData(form, images, "Packages").then(res => {
-            if(res.status === 200) {
+                // close the modal
                 setVisible(false);
-                props.count();
+
+                alert("Package Added Successfully")
+            }
+            else {
+                alert("Package Adding Failed")
             }
         })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const title = e.target[0].value;
+        const price = e.target[1].value;
+        const days = e.target[2].value;
+        const nights = e.target[3].value;
+        const description = e.target[4].value;
+        const resp = uploadImages(e.target[5].files, "packages");
+        resp.then((res) => {
+            const data = {
+                title: title,
+                price: price,
+                days: days,
+                nights: nights,
+                description: description,
+                images: res.data,
+            }
+            handleAdd(data);
+        }
+        )
     }
 
     return (
@@ -35,60 +66,80 @@ export default function PackageAdd(props) {
             </Button>
 
             <Modal
-                scroll
+                scroll={true}
                 width="600px"
                 aria-labelledby="modal-title"
                 aria-describedby="modal-description"
                 {...bindings}
             >
                 <Modal.Header>
-                    <Text id="modal-title" size={20}>
+                    <Text id="modal-title" color="success" css={{
+                        color: "#ffffff",
+                    }} size={20}>
                         Add Package
                     </Text>
                 </Modal.Header>
-                <Modal.Body>
-                    <Grid.Container gap={4}>
-                        <Grid xs={12} lg={12} md={12} sm={12} xl={12}>
-                            <Input
-                                bordered
-                                fullWidth={true}
-                                labelPlaceholder="Title"
-                                onChange={e => setForm({...form, name:e.target.value})}
-                                color="error" />
-                        </Grid>
-                        <Grid  xs={12} lg={12} md={12} sm={12} xl={12}>
-                            <Input
-                                bordered
-                                fullWidth={true}
-                                labelPlaceholder="Stock"
-                                onChange={e => setForm({...form, stock:e.target.value})}
-                                type="number"
-                                color="error" />
-                        </Grid>
-                        
-                        <Grid  xs={12} lg={12} md={12} sm={12} xl={12}>
-                            <Textarea
-                                bordered
-                                onChange={e => setForm({...form, desc:e.target.value})}
-                                fullWidth={true}
-                                labelPlaceholder="Description"
-                                color="error"
-                            />
-                        </Grid>
-                        <Grid>
-                            <input type="file" multiple onChange={e => setImagePath(e.target.files)} />
-                        </Grid>
-                    </Grid.Container>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button auto flat color="error" onPress={() => {setVisible(false), setForm({})}}>
-                        Close
-                    </Button>
-                    <Button auto  color="error" onPress={() => {createProduct(imagePath, form)}}>
-                        Submit
-                    </Button>
-                </Modal.Footer>
+                <form onSubmit={handleSubmit} >
+                    <Modal.Body>
+
+                        <Grid.Container gap={4}>
+                            <Grid xs={12} lg={12} md={12} sm={12} xl={12}>
+                                <Input
+                                    bordered
+                                    fullWidth={true}
+                                    labelPlaceholder="Title"
+
+                                    color="error" />
+                            </Grid>
+                            <Grid xs={12} lg={12} md={12} sm={12} xl={12}>
+                                <Input
+                                    bordered
+                                    fullWidth={true}
+                                    labelPlaceholder="Price"
+                                    type="number"
+                                    color="error" />
+                            </Grid>
+
+                            <Grid xs={12} lg={12} md={12} sm={12} xl={12}>
+                                <Input
+                                    bordered
+                                    fullWidth={true}
+                                    labelPlaceholder="Days"
+                                    type="number"
+                                    color="error" />
+                            </Grid>
+                            <Grid xs={12} lg={12} md={12} sm={12} xl={12}>
+                                <Input
+                                    bordered
+                                    fullWidth={true}
+                                    labelPlaceholder="Nights"
+                                    type="number"
+                                    color="error" />
+                            </Grid>
+
+                            <Grid xs={12} lg={12} md={12} sm={12} xl={12}>
+                                <Textarea
+                                    bordered
+                                    fullWidth={true}
+                                    labelPlaceholder="Description"
+                                    color="error"
+                                />
+                            </Grid>
+                            <Grid>
+                                <input type="file" multiple />
+                            </Grid>
+                        </Grid.Container>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button auto flat color="error" onPress={() => { setVisible(false) }}>
+                            Close
+                        </Button>
+                        <Button auto color="error" type="submit" >
+                            Submit
+                        </Button>
+                    </Modal.Footer>
+                </form>
             </Modal>
-        </div>
+        </div >
     );
 }
--- src/pages/admin/packages/index.js
+++ src/pages/admin/packages/index.js
@@ -1,7 +1,7 @@
 import Layout from '@/admin_components/layout'
 import React, { useEffect, useState } from 'react'
 import styles from "@/styles/admin_styles/products.module.scss"
-import { Table, Row, Col, Tooltip, User, Button } from "@nextui-org/react";
+import { Table, Row, Col, Tooltip, User, Button, Input } from "@nextui-org/react";
 import PlacesAdd from "@/admin_components/model/places/placesAdd";
 import PlacesView from "@/admin_components/model/places/placesView";
 import PlaceEdit from "@/admin_components/model/places/placesEdit";
@@ -10,36 +10,39 @@ import { useRouter } from 'next/router';
 import { ADMIN_ROUTES } from '@/admin_components/core/routes';
 import { useQuery } from '@tanstack/react-query'
 import { getAllData } from '@/utils/firebase_data_handler';
+import PackageAdd from '@/admin_components/model/packages/PackagesAdd';
 function Index() {
 
     const [count, setCount] = useState(0);
     const route = useRouter();
 
 
-    const hotelsData = useQuery(
-        ['hotels'],
+    const packagesData = useQuery(
+        ['packages'],
         () => {
-            return getAllData("Hotels")
+            return getAllData("Packages")
         },
         {
             staleTime: 10000 * 60
         }
     )
 
-    console.log(hotelsData.data?.data);
 
     return (
         <Layout>
             <div className={styles.products} >
                 <div className={styles.head} >
-                    <h3>Hotels</h3>
-                    <Button auto color="success" css={{
+                    <h3>Packages</h3>
+                    {/* <Button auto color="success" css={{
                         color: "#ffffff",
                     }} onClick={() => {
                         route.push(ADMIN_ROUTES.PACKAGES_ADD)
-                    }}> Add Package </Button>
+                    }}> Add Package </Button> */}
+                    <PackageAdd />
                 </div>
                 {/* <Addmodel /> */}
+
+
                 <Table
                     bordered
                     shadow={false}
@@ -61,12 +64,12 @@ function Index() {
                     </Table.Header>
                     <Table.Body>
                         {
-                            // hotelsData.data?.map(({ id, data }, i) => (
-                            hotelsData.data?.data.map((data, i) => (
+                            // packagesData.data?.map(({ id, data }, i) => (
+                            packagesData.data?.data.map((data, i) => (
                                 <Table.Row key={i + 1}>
                                     <Table.Cell>  <User size="xl" squared src={data.images[0]} css={{ p: 0 }}>
                                     </User></Table.Cell>
-                                    <Table.Cell>{data.name}</Table.Cell>
+                                    <Table.Cell>{data.title}</Table.Cell>
 
                                     <Table.Cell>  <Row justify="center" align="center">
                                         <Col css={{ d: "flex" }}>
