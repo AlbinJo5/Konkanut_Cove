35c8390ef0888812715baf9d136ecdeb6821206e
Jacob-alwin <alwinjacobjoy@gmail.com>
2023-05-30 16:30:31 +0530

    oveerflow

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,42 +1,54 @@
-import Layout from "@/components/layout"
-import MainHeader, { MainText, MainTitle } from "@/components/main-header"
-import Link from "next/link"
+import Layout from "@/components/layout";
+import MainHeader, { MainText, MainTitle } from "@/components/main-header";
+import Link from "next/link";
 
-export default function Gallery({text,email,phone}) {
-    return (
-      <Layout>
-          <div className="bg-cover" style={{backgroundImage:"url('/assets/images/back-contact.png')"}}>
-              <div className="py-40 bg-black bg-opacity-20 min-h-screen">
-                  <MainHeader>
-                    <MainTitle width="600px">
-                     Contact Us
-                    </MainTitle>
-                    <MainText width="800px">{text}</MainText>
-                  </MainHeader>
+export default function Gallery({ text, email, phone }) {
+  return (
+    <Layout>
+      <div
+        className="bg-cover"
+        style={{ backgroundImage: "url('/assets/images/back-contact.png')" }}
+      >
+        <div className="py-40 bg-black bg-opacity-20 min-h-screen">
+          <MainHeader>
+            <MainTitle width="600px">Contact Us</MainTitle>
+            <MainText width="800px">{text}</MainText>
+          </MainHeader>
 
-                    <div className="flex max-xs:flex-col text-white justify-center w-full">
-                        <div className="flex flex-col items-center justify-center mt-5 sm:mr-40 mr-20 max-xs:mr-0">
-                            <p className="text-lg mb-3">Email</p>
-                            <Link rel="stylesheet" href={"mailto:"+email} className="bg-green-800 font-bold px-5 py-4 rounded-md" >{email}</Link>
-                        </div>
-                        <div className="flex flex-col items-center mt-5">
-                            <p className="text-lg mb-3">Phone</p>
-                            <Link rel="stylesheet" href={"tel:"+phone} className="bg-green-800 font-bold px-5 py-4 rounded-md" >{email}</Link>
-                        </div>
-                    </div>
-              </div>
-              </div>
-      </Layout>
-    )
-  }
-  
-  export async function getStaticProps(){
-      return {
-          props:{
-            text:"Lorem Ipsum is simply dummy text of the printing and typesetting Lorem Ipsum is simply dummy text of the printing and types Lorem Ipsum is simply dummy text of the printing and typesetting Lorem Ipsum is simply dummy text of the printing and typesLorem Ipsum is simply dummy text of the printing and typesetting Lorem Ipsum is simply dummy text of the printing and types",
-            email:"abcd.xyz.com",
-            phone:"9081456789"
-          }
-      }
-  }
-  
+          <div className="flex max-xs:flex-col text-white justify-center w-full">
+            <div className="flex flex-col items-center justify-center mt-5 sm:mr-40 mr-20 max-xs:mr-0">
+              <p className="text-lg mb-3">Email</p>
+              <Link
+                rel="stylesheet"
+                href={"mailto:" + email}
+                className="bg-green-800 font-bold px-5 text-neutral-50 py-4 rounded-md"
+              >
+                {email}
+              </Link>
+            </div>
+            <div className="flex flex-col items-center mt-5">
+              <p className="text-lg  mb-3">Phone</p>
+              <Link
+                rel="stylesheet"
+                href={"tel:" + phone}
+                className="bg-green-800 text-neutral-50 font-bold px-5 py-4 rounded-md"
+              >
+                {email}
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      text: "Lorem Ipsum is simply dummy text of the printing and typesetting Lorem Ipsum is simply dummy text of the printing and types Lorem Ipsum is simply dummy text of the printing and typesetting Lorem Ipsum is simply dummy text of the printing and typesLorem Ipsum is simply dummy text of the printing and typesetting Lorem Ipsum is simply dummy text of the printing and types",
+      email: "abcd.xyz.com",
+      phone: "9081456789",
+    },
+  };
+}
--- src/pages/packages/index.js
+++ src/pages/packages/index.js
@@ -6,10 +6,7 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { Fade } from "react-reveal";
 
-
 export default function Packages() {
-
-
   const placesData = useQuery(
     ["places"],
     () => {
@@ -18,62 +15,59 @@ export default function Packages() {
     {
       staleTime: 1000 * 60 * 60,
     }
-  )
+  );
   return (
     <Layout>
-      {
-        placesData.isLoading ? (
-          <div className="w-full h-screen flex justify-center items-center">
-            <InitialLoading />
-          </div>
-        ) : (
-          placesData.isError ? (
-            <div className="text-center text-2xl font-bold text-red-500">
-              Error Fetching Data
+      {placesData.isLoading ? (
+        <div className="w-full h-screen flex justify-center items-center">
+          <InitialLoading />
+        </div>
+      ) : placesData.isError ? (
+        <div className="text-center text-2xl font-bold text-red-500">
+          Error Fetching Data
+        </div>
+      ) : (
+        <>
+          <Fade top>
+            <div
+              className="w-full h-[375px] bg-cover contrast-125"
+              style={{ backgroundImage: "url('/assets/images/back22.png')" }}
+            >
+              <div className="w-full h-full bg-black bg-opacity-20 flex flex-col justify-center items-center">
+                <p className="text-6xl text-green-800 font-bold mb-5">
+                  FLEXI PACKAGES
+                </p>
+                <button className="bg-green-800 hover:bg-green-600 text-white text-xl px-4 py-3 rounded-md">
+                  Book Now {"\u2794"}
+                </button>
+              </div>
             </div>
-          ) : (
-            <>
-              <Fade top>
-                <div
-                  className="w-full h-[375px] bg-cover contrast-125"
-                  style={{ backgroundImage: "url('/assets/images/back22.png')" }}
-                >
-                  <div className="w-full h-full bg-black bg-opacity-20 flex flex-col justify-center items-center">
-                    <p className="text-6xl text-green-800 font-bold mb-5">
-                      FLEXI PACKAGES
-                    </p>
-                    <button className="bg-green-800 hover:bg-green-600 text-white text-xl px-4 py-3 rounded-md">
-                      Book Now {"\u2794"}
-                    </button>
-                  </div>
-                </div>
-              </Fade>
-              <ul className="w-full  flex flex-col justify-center align-center items-center m-0 p-0	  my-5  ">
-                {placesData.data?.data
-                  // make the content of the array 3 times
+          </Fade>
+          <ul className="w-full  flex flex-col justify-center align-center items-center m-0 p-0	  my-5  ">
+            {placesData.data?.data
+              // make the content of the array 3 times
 
-                  .map((data, index) => (
-                    <li className="my-5  max-w-fit    " key={index}>
-                      <Fade {...(index % 2 == 0 ? { left: true } : { right: true })}>
-                        <CarouselLarge
-                          data={data.images[0]}
-                          id={data.id}
-                          title={data.title}
-                          desc={"lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla"}
-                          isLeft={index % 2 == 0}
-                          key={index}
-                        />
-                      </Fade>
-                    </li>
-                  ))}
-              </ul>
-            </>
-          )
-        )
-      }
-
+              .map((data, index) => (
+                <li className="my-5  overflow-hidden    " key={index}>
+                  <Fade
+                    {...(index % 2 == 0 ? { left: true } : { right: true })}
+                  >
+                    <CarouselLarge
+                      data={data.images[0]}
+                      id={data.id}
+                      title={data.title}
+                      desc={
+                        "lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla"
+                      }
+                      isLeft={index % 2 == 0}
+                      key={index}
+                    />
+                  </Fade>
+                </li>
+              ))}
+          </ul>
+        </>
+      )}
     </Layout>
   );
 }
-
-
